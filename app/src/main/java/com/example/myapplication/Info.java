0406c0522d9bc002ae5f55446604b8228a6db4fa
Zain-S <saeedzain20@gmail.com>
2021-11-11 09:36:40 +0500

    2nd Page Complete

--- app/src/main/java/com/example/myapplication/Info.java
+++ app/src/main/java/com/example/myapplication/Info.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Info extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class Info extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        TextView name = findViewById(R.id.textView);
+        TextView password = findViewById(R.id.textView3);
+        Bundle extras = getIntent().getExtras();
+        name.setText("UserName: " + extras.getString("etUserName"));
+        password.setText("Password: " + extras.getString("etPassword"));
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,11 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.BT_Submit);
-        EditText etName = findViewById(R.id.ET_Username);
+        EditText etUserName = findViewById(R.id.ET_Username);
+        EditText etPassword = findViewById(R.id.ET_Password);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), Info.class);
+                intent.putExtra("etUserName", etUserName.getText().toString());
+                intent.putExtra("etPassword", etPassword.getText().toString());
                 startActivity(intent);
             }
         });
