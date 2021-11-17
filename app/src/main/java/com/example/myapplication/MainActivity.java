a656b674664a6aaafffccd76a09772a8562845ac
Zain <saeedzain20@gmail.com>
2021-11-17 23:08:20 +0500

    Added log.d

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), Info.class);
                 intent.putExtra("etUserName", etUserName.getText().toString());
                 intent.putExtra("etPassword", etPassword.getText().toString());
+                Log.d("DataSaved", "UserName: " + etUserName.getText().toString());
+                Log.d("DataSaved", "Password: " + etPassword.getText().toString());
                 startActivity(intent);
             }
         });
